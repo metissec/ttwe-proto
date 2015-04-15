d1743eccf98ddd0ce5e4971a29058aa1875f6112
James <metissec@users.noreply.github.com>
2015-04-15 16:41:57 -0700

    Update TTWEControl.py

--- TTWEControl.py
+++ TTWEControl.py
@@ -1,16 +1,7 @@
-def setup_host():
+def setup_host(verbose=False, out=1, in_1=2, in_2=3):
   
   signal.signal(signal.SIGINT, signal_handler)
-
-  parser = argparse.ArgumentParser()
-  parser.add_argument("-v", "--verbose", action="store_true", 
-      help="turn on verbose output of USB communication")
-  parser.add_argument("--OUT", type=int, default=1, help="peripheral OUT Endpoint number")
-  parser.add_argument("--IN", type=int, default=2, help="peripheral IN Endpoint number")
-  parser.add_argument("--IN2", type=int, default=3, help="peripheral IN2 Endpoint number")
-
-  args = parser.parse_args()
-
+  
   #Initialize FET and set baud rate
   client=GoodFETMAXUSBHost();
   client.serInit()
@@ -19,24 +10,22 @@ def setup_host():
 
   print('host init')
   # hub detects device
-  client.hostinit(HostRelayDevice(args.verbose, OUT_EP=args.OUT, 
-    IN_EP=args.IN, IN2_EP=args.IN2)); 
+  client.hostinit(HostRelayDevice(verbose, OUT_EP=out, IN_EP=in_1, IN2_EP=in_2)); 
   client.usbverbose=False;
 
   print('device detect')
+
   # detect the device-low or full speed
   client.detect_device(); 
   time.sleep(0.2); 
 
   # reset bus, set address 0 
   client.soft_enumerate() 
-
   client.reset_bus()
-
-  print('done enumerating, deferring to irqs')
   client.service_irqs()
   client.usb_disconnect()
   
+
 def setup_client():
   
   signal.signal(signal.SIGINT, signal_handler)
