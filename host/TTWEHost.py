d03bb686c08bc556d0dde0b5c98a0ae8ec03ca39
James <metissec@users.noreply.github.com>
2015-04-16 12:35:49 -0700

    Update TTWEHost.py

--- host/TTWEHost.py
+++ host/TTWEHost.py
@@ -138,3 +138,42 @@ class HostRelayDevice:
       self.snd_ep3.write(self.snd_ep3_data)
       self.snd_ep3.flush()
       self.snd_ep3_data = '\n'
+
+if __name__ == '__main__':
+
+  signal.signal(signal.SIGINT, signal_handler)
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument("-v", "--verbose", action="store_true", 
+      help="turn on verbose output of USB communication")
+  parser.add_argument("--OUT", type=int, default=1, help="peripheral OUT Endpoint number")
+  parser.add_argument("--IN", type=int, default=2, help="peripheral IN Endpoint number")
+  parser.add_argument("--IN2", type=int, default=3, help="peripheral IN2 Endpoint number")
+
+  args = parser.parse_args()
+
+  #Initialize FET and set baud rate
+  client=GoodFETMAXUSBHost();
+  client.serInit()
+
+  client.MAXUSBsetup();
+
+  print('host init')
+  # hub detects device
+  client.hostinit(HostRelayDevice(args.verbose, OUT_EP=args.OUT, 
+    IN_EP=args.IN, IN2_EP=args.IN2)); 
+  client.usbverbose=False;
+
+  print('device detect')
+  # detect the device-low or full speed
+  client.detect_device(); 
+  time.sleep(0.2); 
+
+  # reset bus, set address 0 
+  client.soft_enumerate() 
+
+  client.reset_bus()
+
+  print('done enumerating, deferring to irqs')
+  client.service_irqs()
+  client.usb_disconnect()
