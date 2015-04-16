6c9184b2342e0853cad4e9119a197ccf88d655d0
James <metissec@users.noreply.github.com>
2015-04-16 12:44:35 -0700

    Update GoodFET.py

--- host/GoodFET.py
+++ host/GoodFET.py
@@ -24,6 +24,81 @@ def getClient(name="GoodFET"):
     print("Unsupported target: %s" % name)
     sys.exit(0)
 
+class SymbolTable:
+    """GoodFET Symbol Table"""
+    db=sqlite3.connect(":memory:")
+    
+    def __init__(self, *args, **kargs):
+        self.db.execute("create table if not exists symbols(adr,name,memory,size,comment);")
+    def get(self,name):
+        self.db.commit()
+        c=self.db.cursor()
+        try:
+            c.execute("select adr,memory from symbols where name=?",(name,))
+            for row in c:
+                #print "Found it."
+                sys.stdout.flush()
+                return row[0]
+            #print "No dice."
+        except:# sqlite3.OperationalError:
+            #print "SQL error."
+            return eval(name)
+        return eval(name)
+    def define(self,adr,name,comment="",memory="vn",size=16):
+        self.db.execute("insert into symbols(adr,name,memory,size,comment)"
+                        "values(?,?,?,?,?);", (
+                adr,name,memory,size,comment))
+        #print "Set %s=%s." % (name,adr)
+class GoodFETbtser:
+    """py-bluez class for emulating py-serial."""
+    def __init__(self,btaddr):
+        import bluetooth
+        if btaddr==None or btaddr=="none" or btaddr=="bluetooth":
+            print("performing inquiry...")
+            nearby_devices = bluetooth.discover_devices(lookup_names = True)
+            print("found %d devices" % len(nearby_devices))
+            for addr, name in nearby_devices:
+                print("  %s - '%s'" % (addr, name))
+                #TODO switch to wildcards.
+                if name=='FireFly-A6BD':
+                    btaddr=addr
+                if name=='RN42-A94A':
+                    btaddr=addr
+                
+            print("Please set $GOODFET to the address of your device.")
+            sys.exit()
+        print("Identified GoodFET at %s" % btaddr)
+
+        # Manually use the portnumber.
+        port=1
+        
+        print("Connecting to %s on port %i." % (btaddr, port))
+        sock=bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+        self.sock=sock
+        sock.connect((btaddr,port))
+        sock.settimeout(10);  #IMPORTANT Must be patient.
+        
+        ##This is what we'd do for a normal reset.
+        #str=""
+        #while not str.endswith("goodfet.sf.net/"):
+        #    str=self.read(64)
+        #    print(str)
+        
+        # Instead, just return and hope for the best.
+        return
+        
+    def write(self,msg):
+        """Send traffic."""
+        import time
+        self.sock.send(msg)
+        #time.sleep(0.1)
+        return
+    def read(self,length):
+        """Read traffic."""
+        data=""
+        while len(data)<length:
+            data=data+self.sock.recv(length-len(data))
+        return data
 class GoodFET:
     """GoodFET Client Library"""
 
@@ -50,10 +125,18 @@ class GoodFET:
     def serInit(self, port=None, timeout=2, attemptlimit=None):
         """Open a serial port of some kind."""
         import re
+        
         if port==None:
             port=os.environ.get("GOODFET")
+        if port=="bluetooth" or (port is not None and re.match("..:..:..:..:..:..",port)):
+            self.btInit(port,2,attemptlimit)
         else:
             self.pyserInit(port,timeout,attemptlimit)
+    def btInit(self, port, timeout, attemptlimit):
+        """Open a bluetooth port."""
+        #self.verbose=True;  #For debugging BT.
+        self.serialport=GoodFETbtser(port)
+        
     def pyserInit(self, port, timeout, attemptlimit):
         """Open the serial port"""
         # Make timeout None to wait forever, 0 for non-blocking mode.
@@ -182,8 +265,152 @@ class GoodFET:
         self.serialport.setTimeout(12)
     def serClose(self):
         self.serialport.close()
-    def f(self, x): 
-        return str(int(bin(x)[:-3], 2)) + (' %s' % 'w' if int(bin(x)[-2]) == 1 else ' r')
+    def telosSetSCL(self, level):
+        self.serialport.setRTS(not level)
+    def telosSetSDA(self, level):
+        self.serialport.setDTR(not level)
+
+    def telosI2CStart(self):
+        self.telosSetSDA(1)
+        self.telosSetSCL(1)
+        self.telosSetSDA(0)
+
+    def telosI2CStop(self):
+        self.telosSetSDA(0)
+        self.telosSetSCL(1)
+        self.telosSetSDA(1)
+
+    def telosI2CWriteBit(self, bit):
+        self.telosSetSCL(0)
+        self.telosSetSDA(bit)
+        time.sleep(2e-6)
+        self.telosSetSCL(1)
+        time.sleep(1e-6)
+        self.telosSetSCL(0)
+
+    def telosI2CWriteByte(self, byte):
+        self.telosI2CWriteBit( byte & 0x80 )
+        self.telosI2CWriteBit( byte & 0x40 )
+        self.telosI2CWriteBit( byte & 0x20 )
+        self.telosI2CWriteBit( byte & 0x10 )
+        self.telosI2CWriteBit( byte & 0x08 )
+        self.telosI2CWriteBit( byte & 0x04 )
+        self.telosI2CWriteBit( byte & 0x02 )
+        self.telosI2CWriteBit( byte & 0x01 )
+        self.telosI2CWriteBit( 0 );  # "acknowledge"
+
+    def telosI2CWriteCmd(self, addr, cmdbyte):
+        self.telosI2CStart()
+        self.telosI2CWriteByte( 0x90 | (addr << 1) )
+        self.telosI2CWriteByte( cmdbyte )
+        self.telosI2CStop()
+    def bslResetZ1(self, invokeBSL=0):
+        '''
+        Applies BSL entry sequence on RST/NMI and TEST/VPP pins
+        Parameters:
+            invokeBSL = 1: complete sequence
+            invokeBSL = 0: only RST/NMI pin accessed
+            
+        By now only BSL mode is accessed
+        '''
+        
+        #if DEBUG > 1: sys.stderr.write("* bslReset(invokeBSL=%s)\n" % invokeBSL)
+        if invokeBSL:
+            #sys.stderr.write("in Z1 bsl reset...\n")
+            time.sleep(0.1)
+            self.writepicROM(0xFF, 0xFF)
+            time.sleep(0.1)
+            #sys.stderr.write("z1 bsl reset done...\n")
+        else:
+            #sys.stderr.write("in Z1 reset...\n")
+            time.sleep(0.1)
+            self.writepicROM(0xFF, 0xFE)
+            time.sleep(0.1)
+            #sys.stderr.write("z1 reset done...\n")
+    def writepicROM(self, address, data):
+        ''' Writes data to @address'''
+        for i in range(7,-1,-1):
+            self.picROMclock((address >> i) & 0x01)
+        self.picROMclock(0)
+        recbuf = 0
+        for i in range(7,-1,-1):
+            s = ((data >> i) & 0x01)
+            #print s
+            if i < 1:
+                r = not self.picROMclock(s, True)
+            else:
+                r = not self.picROMclock(s)
+            recbuf = (recbuf << 1) + r
+
+        self.picROMclock(0, True)
+        #k = 1
+        #while not self.serial.getCTS():
+        #    pass 
+        #time.sleep(0.1)
+        return recbuf
+    def readpicROM(self, address):
+        ''' reads a byte from @address'''
+        for i in range(7,-1,-1):
+            self.picROMclock((address >> i) & 0x01)
+        self.picROMclock(1)
+        recbuf = 0
+        r = 0
+        for i in range(7,-1,-1):
+            r = self.picROMclock(0)
+            recbuf = (recbuf << 1) + r
+        self.picROMclock(r)
+        #time.sleep(0.1)
+        return recbuf
+        
+    #This seems more reliable when slowed.
+    def picROMclock(self, masterout, slow = True):
+        #print "setting masterout to "+str(masterout)
+        self.serialport.setRTS(masterout)
+        self.serialport.setDTR(1)
+        #time.sleep(0.02)
+        self.serialport.setDTR(0)
+        if slow:
+            time.sleep(0.02)
+        return self.serialport.getCTS()
+
+    def picROMfastclock(self, masterout):
+        #print("setting masterout to "+str(masterout))
+        self.serialport.setRTS(masterout)
+        self.serialport.setDTR(1)
+        self.serialport.setDTR(0)
+        time.sleep(0.02)
+        return self.serialport.getCTS()
+
+    def telosBReset(self,invokeBSL=0):
+        # "BSL entry sequence at dedicated JTAG pins"
+        # rst !s0: 0 0 0 0 1 1
+        # tck !s1: 1 0 1 0 0 1
+        #   s0|s1: 1 3 1 3 2 0
+
+        # "BSL entry sequence at shared JTAG pins"
+        # rst !s0: 0 0 0 0 1 1
+        # tck !s1: 0 1 0 1 1 0
+        #   s0|s1: 3 1 3 1 0 2
+
+        if invokeBSL:
+            self.telosI2CWriteCmd(0,1)
+            self.telosI2CWriteCmd(0,3)
+            self.telosI2CWriteCmd(0,1)
+            self.telosI2CWriteCmd(0,3)
+            self.telosI2CWriteCmd(0,2)
+            self.telosI2CWriteCmd(0,0)
+        else:
+            self.telosI2CWriteCmd(0,3)
+            self.telosI2CWriteCmd(0,2)
+
+        # This line was not defined inside the else: block, not sure where it
+        # should be however
+        self.telosI2CWriteCmd(0,0)
+        time.sleep(0.250)       #give MSP430's oscillator time to stabilize
+        self.serialport.flushInput()  #clear buffers
+
+    def f(self, x): return str(int(bin(x)[:-3], 2)) + (' %s' % 'w' if int(bin(x)[-2]) == 1 else ' r')
+
     def getbuffer(self,size=0x1c00):
         writecmd(0,0xC2,[size&0xFF,(size>>16)&0xFF])
         print("Got %02x%02x buffer size." % (self.data[1],self.data[0]))
@@ -292,6 +519,45 @@ class GoodFET:
                     #print("This shouldn't happen after syncing.  Exiting for safety.";                    )
                     #sys.exit(-1)
                 return self.data
+    #Glitching stuff.
+    def glitchApp(self,app):
+        """Glitch into a device by its application."""
+        self.data=[app&0xff]
+        self.writecmd(self.GLITCHAPP,0x80,1,self.data)
+        #return ord(self.data[0])
+    def glitchVerb(self,app,verb,data):
+        """Glitch during a transaction."""
+        if data==None: data=[]
+        self.data=[app&0xff, verb&0xFF]+data
+        self.writecmd(self.GLITCHAPP,0x81,len(self.data),self.data)
+        #return ord(self.data[0])
+    def glitchstart(self):
+        """Glitch into the AVR application."""
+        self.glitchVerb(self.APP,0x20,None)
+    def glitchstarttime(self):
+        """Measure the timer of the START verb."""
+        return self.glitchTime(self.APP,0x20,None)
+    def glitchTime(self,app,verb,data):
+        """Time the execution of a verb."""
+        if data==None: data=[]
+        self.data=[app&0xff, verb&0xFF]+data
+        print("Timing app %02x verb %02x." % (app,verb))
+        self.writecmd(self.GLITCHAPP,0x82,len(self.data),self.data)
+        time=ord(self.data[0])+(ord(self.data[1])<<8)
+        print("Timed to be %i." % time)
+        return time
+    def glitchVoltages(self,low=0x0880, high=0x0fff):
+        """Set glitching voltages. (0x0fff is max.)"""
+        self.data=[low&0xff, (low>>8)&0xff,
+                   high&0xff, (high>>8)&0xff]
+        self.writecmd(self.GLITCHAPP,0x90,4,self.data)
+        #return ord(self.data[0])
+    def glitchRate(self,count=0x0800):
+        """Set glitching count period."""
+        self.data=[count&0xff, (count>>8)&0xff]
+        self.writecmd(self.GLITCHAPP,0x91,2,
+                      self.data)
+        #return ord(self.data[0])
     
     
     #Monitor stuff
